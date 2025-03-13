5453829be1dd92c9a60d47ac41248762dff2e26c
margot <margot@thinkr.fr>
2025-03-13 11:50:12 +0100

    fix: remove check columns in DE

--- R/check_expected_colnames.R
+++ R/check_expected_colnames.R
@@ -1,48 +1,24 @@
 #' Check that mandatory columns are present in the dataset
 #'
 #' @param data Data frame. Raw data about projects.
-#' @param lang Character. Language, either "fr" or "de".
 #'
 #' @importFrom glue glue
 #' @return An error if some colnames are absent.
 check_expected_colnames <- function(
-    data,
-    lang = c("fr", "de")
+    data
     ) {
 
-  lang <- match.arg(lang)
-
-  if (lang == "fr") {
-
-    vec_expected_colnames <- c(
-      "Fiche d\xE2\x80\x99informations ID",
-      "Nom",
-      "Description",
-      "Niveau",
-      "I", "II", "III",
-      "A", "B", "C", "D",
-      "Th\xC3\xA8me",
-      "Groupe cible",
-      "Setting"
-    )
-
-  }
-
-  if (lang == "de") {
-
-    vec_expected_colnames <- c(
-      "Steckbrief ID",
-      "Titel",
-      "Beschreibung",
-      "Ebene",
-      "I", "II", "III",
-      "A", "B", "C", "D",
-      "Thema",
-      "Zielgruppe",
-      "Setting"
-    )
-
-  }
+  vec_expected_colnames <- c(
+    "Fiche d\xE2\x80\x99informations ID",
+    "Nom",
+    "Description",
+    "Niveau",
+    "I", "II", "III",
+    "A", "B", "C", "D",
+    "Th\xC3\xA8me",
+    "Groupe cible",
+    "Setting"
+  )
 
   if (!all(vec_expected_colnames %in% colnames(data))) {
     stop(
--- tests/testthat/test-check_expected_colnames.R
+++ tests/testthat/test-check_expected_colnames.R
@@ -1,53 +1,17 @@
-test_that("check_expected_colnames works in FR", {
+test_that("check_expected_colnames works", {
 
   data("data_raw_fr")
 
   expect_error(
     check_expected_colnames(
-      data_raw_fr,
-      lang = "fr"
+      data_raw_fr
     ),
     NA
   )
 
   expect_error(
     check_expected_colnames(
-      data_raw_fr |> dplyr::select(-Nom),
-      lang = "fr"
-    )
-  )
-
-})
-
-test_that("check_expected_colnames works in DE", {
-
-  data("data_raw_de")
-
-  expect_error(
-    check_expected_colnames(
-      data_raw_de,
-      lang = "de"
-    ),
-    NA
-  )
-
-  expect_error(
-    check_expected_colnames(
-      data_raw_de |> dplyr::select(-Nom),
-      lang = "de"
-    )
-  )
-
-})
-
-test_that("check_expected_colnames does not work for another language", {
-
-  data("data_raw_fr")
-
-  expect_error(
-    check_expected_colnames(
-      data_raw_fr,
-      lang = "it"
+      data_raw_fr |> dplyr::select(-Nom)
     )
   )
 
