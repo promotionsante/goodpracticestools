ed131fee345118353a5598201388b765c28520a0
margot <margot@thinkr.fr>
2025-03-13 12:33:15 +0100

    fix: use a constant for mandatory columns

--- tests/testthat/test-check_expected_col.R
+++ tests/testthat/test-check_expected_col.R
@@ -1,16 +1,16 @@
-test_that("check_expected_colnames works", {
+test_that("check_expected_col works", {
 
   data("data_raw_fr")
 
   expect_error(
-    check_expected_colnames(
+    check_expected_col(
       data_raw_fr
     ),
     NA
   )
 
   expect_error(
-    check_expected_colnames(
+    check_expected_col(
       data_raw_fr |> dplyr::select(-Nom)
     )
   )
