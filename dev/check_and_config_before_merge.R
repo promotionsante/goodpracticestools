3fc291136fe3141a6c2e12dc85ed661634eebc6c
margot <margot@thinkr.fr>
2025-03-17 16:37:51 +0100

    doc: add the coverage report in pkgdown

--- dev/check_and_config_before_merge.R
+++ dev/check_and_config_before_merge.R
@@ -1,7 +1,12 @@
 # Check
 attachment::att_amend_desc()
 pkgload::load_all()
-devtools::test()
+devtools::check()
 
 # Pkgdown
 pkgdown::build_site()
+
+# Coverage report
+covr::report(
+  file = file.path("docs/articles/coverage-report.html")
+)
